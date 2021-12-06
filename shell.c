ad3307d55957b56d50d7924db67abd6d9828ac8d
Erim Vurucu <55758293+ErimV@users.noreply.github.com>
2021-12-06 21:19:09 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/wait.h>
 #include <unistd.h>
-#include <string.h>
 
 void prompt(){
-	char cwd[1024];
+    char cwd[1024];
     getcwd(cwd, sizeof(cwd));
     char *username=getlogin();
     printf("\033[0;33m");
-	printf("%s/: %s ==>\n ",cwd,username);
-	printf("\033[0m");
+    printf("%s/: %s ==>\n ",cwd,username);
+    printf("\033[0m");
 }
 
 
 
 int main()
 {
-	//while(1){
-		prompt();
-	//}
+    //while(1){
+	prompt();
+    //}
 }
