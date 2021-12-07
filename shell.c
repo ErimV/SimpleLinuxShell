cacca006f140b26a886d2963edaac0ab9e07251c
Erim Vurucu <55758293+ErimV@users.noreply.github.com>
2021-12-07 20:14:11 +0300

    Update shell.c
    Geçici olarak kullanıcı girişini alan ve girişi kelimelere ayıran fonksiyon eklendi.

--- shell.c
+++ shell.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 void prompt(){
-    char cwd[1024];
+	char cwd[1024];
     getcwd(cwd, sizeof(cwd));
-    char *username=getlogin();
-    printf("\033[0;33m");
-    printf("%s/: %s ==>\n ",cwd,username);
-    printf("\033[0m");
+   	char *username=getlogin();
+   	printf("\033[0;33m");
+	printf("%s/: %s ==> ",cwd,username);
+	printf("\033[0m");
 }
 
-
+void user_input(){
+	char inp[81],*word,*prgname,*param;
+	int count=0;
+	fgets(inp,81,stdin);
+	char *ptrinp=strdup(inp);
+	while( (word = strsep(&ptrinp," ")) != NULL ){
+       	count++;
+       	if(count==1) prgname=word;
+       	else param=word;
+	}
+}
 
 int main()
 {
-    //while(1){
-	prompt();
-    //}
+	//while(1){
+		prompt();
+		user_input();
+	//}
 }
